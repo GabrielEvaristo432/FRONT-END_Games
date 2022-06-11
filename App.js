ec269d9bc4ee1c250668375e95803025441b9f96
Renato Alencar <renattoareval@gmail.com>
2022-06-10 20:36:40 -0400

    Iniciando a codificação da paginação do app

--- App.js
+++ App.js
@@ -1,7 +1,51 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import React, { Component } from "react";
+import * as React from 'react';
+import { Component } from 'react';
 import api from './src/services/api';
 import Games from './src/Games';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+class Home extends Component {
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>Bem vindo à Íris</Text>
+      </View>
+    )
+  }
+}
+
+const jogos = [
+  {
+    nome: "Sonic Unleashed",
+    metascore: 60,
+    userscore: 81
+  },
+  {
+    nome: "Metal Slug 3",
+    metascore: 80,
+    userscore: 91
+  }
+] 
+
+class Jogos extends Component {
+  
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>Jogos</Text>
+        <FlatList
+          data={jogos}
+          keyExtractor={(item) => item.nome}
+          renderItem={ ({item}) => <Games data={item} /> }
+        />
+      </View>
+    )
+  }
+}
+
+const Stack = createNativeStackNavigator();
 
 class App extends Component {
   constructor(props){
@@ -11,23 +55,25 @@ class App extends Component {
     }
   }
 
+  
+
   async componentDidMount(){
-    const response = await api.get('/games');
+    const response = await api.get('/GabrielEvaristo432');
     this.setState({
       games: response.data
     });
   }
 
+  
+
   render(){
     return(
-      <View>
-        <Text>Jogos</Text>
-        <FlatList
-          data={this.state.games}
-          keyExtractor={this.state.games.nome}
-          
-        />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* <Stack.Screen name="Home" component={Home} /> */}
+          <Stack.Screen name="Jogos" component={Jogos} />
+        </Stack.Navigator>
+      </NavigationContainer>
     )
   }
 }
